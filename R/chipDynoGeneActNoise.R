1ee7ba69d20266a427a0629c9a6062d3d6c82dc0
marahman <arifmail@gmail.com>
2013-07-30 14:32:32 +0100

    GeneActNoise: activators in decreasing order

--- R/chipDynoGeneActNoise.R
+++ R/chipDynoGeneActNoise.R
@@ -1,17 +1,25 @@
-#function [list,maxActivity,maxActivityError]=chipDynoGeneAct(data, ...
-#                                                  X,Sigma,beta,gamma,mu, ...
-#                                                  transNames, ...
-#                                                  annotation,geneName);
-#
-#% CHIPDYNOGENEACT given a gene, lists activators in decreasing order
-#%
-#%	Description:
-#%	[list,maxActivity,maxActivityError]=chipDynoGeneAct(data, ...
-#%                                                  X,Sigma,beta,gamma,mu, ...
-#%                                                  transNames, ...
-#%                                                  annotation,geneName);
-#%% 	chipDynoGeneAct.R version 0.1.0
-#
+# CHIPDYNOGENEACTNOISE given a gene, lists activators in decreasing order
+# CHIPDYNO toolbox
+# chipDynoGeneActNoise.R version 1.0.1
+# FORMAT chipDynoGeneAct <- function(data, X, Sigma,beta,precs, gamma,mu, 
+#					transNames, annotation, geneName);
+# DESC given a gene, lists activators in decreasing order
+# ARG data : point estimate of the expression level
+# ARG X : connectivity measurement between genes and transcription factors
+# ARG Sigma : prior covariance matrix of TFA
+# ARG beta :
+# ARG precs :
+# ARG gamma : degree of temporal continuity
+# ARG mu : mean value of the transcription factor activity
+# ARG transNames : Transcription factors
+# ARG annotation : Gene names
+# ARG geneName : specific gene name
+# RETURN list : concatenated data frame of lists activators for a given gene 
+# in decreasing order, maximum activity and maximum activity error
+# COPYRIGHT : Neil D. Lawrence, 2006
+# COPYRIGHT : Guido Sanguinetti, 2006
+# MODIFICATIONS : Muhammad A. Rahman, 2013
+# SEEALSO : chipDynoGeneAct, chipDynoActTransFact, chipDynoActTransFactNoise
 
 chipDynoGeneActNoise = function(data, X, Sigma, beta, precs, gamma,mu, transNames, annotation, geneName) {
 
@@ -33,12 +41,6 @@ nTransFact=sum(X[I,]);
 maxActivity=list();
 maxActivityError=list();
 
-#I=find(strcmp(geneName,annotation));
-#activeNames=transNames(find(X(I,:)));
-#nTransFact=sum(X(I,:));
-#maxActivity=[];
-#maxActivityError=[];
-
 source("chipDynoExpectationsFastNoise.R")
 
 for (i in 1: nTransFact) {
@@ -55,26 +57,11 @@ for (i in 1: nTransFact) {
 	maxActivityError=cbind(maxActivityError,tfError[index]);
 }
 
-#for i=1:nTransFact
-#  [tf,tfError]=chipDynoExpectationsFast(data,X,Sigma,beta,gamma,mu, ...
-#                                         transNames, annotation, ...
-#                                         activeNames(i),geneName);
-#  ind=find(strcmp(activeNames(i),transNames));
-#  tf=tf-mu(ind)*ones(size(tf));
-#  [act,index]=max(tf);
-#  maxActivity=[maxActivity,act];
-#  maxActivityError=[maxActivityError,tfError(index)];
-#end
-
 temp_ind = sort(unlist(maxActivity), decreasing = TRUE, index.return = TRUE)
 maxActivity = temp_ind$x # '$x' return the sorted value 
 index = temp_ind$ix	# '$ix' return the sorted value's index
 maxActivityError=unlist(maxActivityError[index]);
 list=activeNames[index];
-
-#[maxActivity,index]=sort(maxActivity,'descend');
-#maxActivityError=maxActivityError(index);
-#list=activeNames(index);
 
 name_value_error = list(list,maxActivity,maxActivityError)
 return(name_value_error)
